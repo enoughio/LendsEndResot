6e31124e67ee993b40e5cba3bfaf07403b43bc8b
Ani <turboji007@gmail.com>
2025-11-21 11:56:57 +0530

    feat: added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${urbanist.className}`}>
-
+        <Analytics />
         {children}
         
       </body>
